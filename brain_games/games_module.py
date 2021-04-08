8ee9bde43940bc6d659c793659ef8dabe74e71b0
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-08 23:56:36 +0300

    Refactore games_module after lint

--- brain_games/games_module.py
+++ brain_games/games_module.py
@@ -5,7 +5,8 @@ import random
 
 import prompt
 
-RIGTH_ANSWER_NEEDED = 3
+RIGHT_ANSWER_NEEDED = 3
+RANDOM_CALC_MAX = 20
 
 
 def welcome_user():
@@ -19,7 +20,7 @@ def even_game(user_name):
     """Check user knowlenge about even numbers."""
     print('Answer "yes" if the number is even, otherwise answer "no".')
     right_answer_count = 0
-    while right_answer_count < RIGTH_ANSWER_NEEDED:
+    while right_answer_count < RIGHT_ANSWER_NEEDED:
         random_number = random.randint(0, 100)
         if random_number % 2 == 0:
             right_answer = 'yes'
@@ -39,15 +40,20 @@ def even_game(user_name):
 
 def calc_game(user_name):
     """Check user knowlenge in simple math."""
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('What is the result of the expression?')
     right_answer_count = 0
-    while right_answer_count < RIGTH_ANSWER_NEEDED:
-        random_number = random.randint(0, 100)
-        if random_number % 2 == 0:
-            right_answer = 'yes'
-        else:
-            right_answer = 'no'
-        print('Question: {0}'.format(random_number))
+    while right_answer_count < RIGHT_ANSWER_NEEDED:
+        random_number1 = random.randint(0, RANDOM_CALC_MAX)
+        random_number2 = random.randint(0, RANDOM_CALC_MAX)
+        random_operation = random.choice(['+', '-', '*'])
+        if random_operation == '+':
+            right_answer = random_number1 + random_number2
+        elif random_operation == '-':
+            right_answer = random_number1 - random_number2
+        elif random_operation == '*':
+            right_answer = random_number1 * random_number2
+        right_answer = str(right_answer)
+        print('Question: {0} {1} {2}'.format(random_number1, random_operation, random_number2))
         user_answer = prompt.string('Your answer: ')
         if user_answer != right_answer:
             print("'{0}' is wrong answer ;(. Correct answer was '{1}'".format(user_answer, right_answer))
