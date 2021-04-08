12564072bc94354feb1b68cc532bcd323c396b88
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-09 00:34:36 +0300

    Write gcd game

--- brain_games/games_module.py
+++ brain_games/games_module.py
@@ -7,6 +7,7 @@ import prompt
 
 RIGHT_ANSWER_NEEDED = 3
 RANDOM_CALC_MAX = 20
+RANDOM_GCD_MAX = 50
 
 
 def welcome_user():
@@ -54,6 +55,41 @@ def calc_game(user_name):
             right_answer = random_number1 * random_number2
         right_answer = str(right_answer)
         print('Question: {0} {1} {2}'.format(random_number1, random_operation, random_number2))
+        user_answer = prompt.string('Your answer: ')
+        if user_answer != right_answer:
+            print("'{0}' is wrong answer ;(. Correct answer was '{1}'".format(user_answer, right_answer))
+            print("Let's try again, {0}!".format(user_name))
+            right_answer_count = 0
+            continue
+        print('Correct!')
+        right_answer_count += 1
+    print('Congratulations, {0}!'.format(user_name))
+
+
+def find_gcd(num1, num2):
+    """Find GCD."""
+    if num1 == num2:
+        return num1
+    if num1 > num2:
+        gcd = num2
+    else:
+        gcd = num1
+    while gcd != 1:
+        if num1 % gcd == 0 and num2 % gcd == 0:
+            break
+        gcd -= 1
+    return gcd
+
+
+def gcd_game(user_name):
+    """Check user knowlenge in simple math."""
+    print('Find the greatest common divisor of given numbers.')
+    right_answer_count = 0
+    while right_answer_count < RIGHT_ANSWER_NEEDED:
+        random_number1 = random.randint(0, RANDOM_GCD_MAX)
+        random_number2 = random.randint(0, RANDOM_GCD_MAX)
+        right_answer = str(find_gcd(random_number1, random_number2))
+        print('Question: {0} {1}'.format(random_number1, random_number2))
         user_answer = prompt.string('Your answer: ')
         if user_answer != right_answer:
             print("'{0}' is wrong answer ;(. Correct answer was '{1}'".format(user_answer, right_answer))
