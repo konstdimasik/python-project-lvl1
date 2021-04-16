0fc7cb65ed5b48a4959dc4724c706fca38163560
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-16 22:34:11 +0300

    Refactor gcd game

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,5 +1,10 @@
 """GCD check."""
 
+import random
+
+RANDOM_GCD_MAX = 50
+RULES = 'Find the greatest common divisor of given numbers.'
+
 
 def find_gcd(num1, num2):
     """Find GCD."""
@@ -14,3 +19,12 @@ def find_gcd(num1, num2):
             break
         gcd -= 1
     return gcd
+
+
+def generate_level():
+    """GCD game core."""
+    random_number1 = random.randint(1, RANDOM_GCD_MAX)
+    random_number2 = random.randint(1, RANDOM_GCD_MAX)
+    right_answer = str(find_gcd(random_number1, random_number2))
+    question = 'Question: {0} {1}'.format(random_number1, random_number2)
+    return (question, right_answer)
