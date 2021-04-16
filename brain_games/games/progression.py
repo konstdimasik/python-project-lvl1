663d42379095826be881471f1db93c5748339ba8
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-16 22:57:53 +0300

    Refactor progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,6 +2,8 @@
 
 import random
 
+RULES = 'What number is missing in the progression?'
+
 
 def make_progression():
     """Make progression with random length start and step."""
@@ -12,3 +14,17 @@ def make_progression():
     for i in range(length):
         numbers.append(start + step * i)
     return numbers
+
+
+def generate_level():
+    """Progression game core."""
+    progression = make_progression()
+    random_position = random.randint(0, len(progression) - 1)
+    right_answer = str(progression[random_position])
+    question = 'Question: '
+    for i, num in enumerate(progression):
+        if i == random_position:
+            question += '.. '
+        else:
+            question += '{0} '.format(num)
+    return (question, right_answer)
