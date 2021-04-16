bdeee3d058c7dd9575a5a432c5a3b251ade5336c
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-16 22:41:42 +0300

    Refactor prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,5 +1,10 @@
 """Prime check."""
 
+import random
+
+MAX_CHECK_NUM = 100
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
 
 def is_prime(num):
     """Check num for prime."""
@@ -9,3 +14,14 @@ def is_prime(num):
     while divider * divider <= num and num % divider != 0:
         divider += 2
     return divider * divider > num
+
+
+def generate_level():
+    """Prime game core."""
+    random_number = random.randint(2, MAX_CHECK_NUM)
+    if is_prime(random_number):
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
+    question = 'Question: {0}'.format(random_number)
+    return (question, right_answer)
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,38 +1,13 @@
 #!/usr/bin/env python
 """Prime programm."""
 
-import random
-
-import prompt
-from brain_games.games.prime import is_prime
-from brain_games.games_module import (
-    RIGHT_ANSWER_NEEDED,
-    check_answer,
-    welcome_user,
-)
-
-MAX_CHECK_NUM = 100
+from brain_games.games import prime
+from brain_games.games_module import engine
 
 
 def main():
     """Check user knowlenge in prime numbers."""
-    print('Welcome to the Brain Games!')
-    user_name = welcome_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    right_answer_count = 0
-    while right_answer_count < RIGHT_ANSWER_NEEDED:
-        random_number = random.randint(2, MAX_CHECK_NUM)
-        if is_prime(random_number):
-            right_answer = 'yes'
-        else:
-            right_answer = 'no'
-        print('Question: {0}'.format(random_number))
-        user_answer = prompt.string('Your answer: ')
-        if check_answer(user_answer, right_answer, user_name):
-            right_answer_count += 1
-        else:
-            return
-    print('Congratulations, {0}!'.format(user_name))
+    engine(prime)
 
 
 if __name__ == '__main__':
