51375b74b692ebe0445820bac648f039e279267b
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-16 22:26:50 +0300

    Refactor calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,10 @@
 """Calculation check."""
 
+import random
+
+RANDOM_CALC_MAX = 20
+RULES = 'What is the result of the expression?'
+
 
 def get_result(num1, num2, operation):
     """Find result of math operation."""
@@ -9,3 +14,13 @@ def get_result(num1, num2, operation):
         return num1 - num2
     elif operation == '*':
         return num1 * num2
+
+
+def generate_level():
+    """Calc game core."""
+    rndm_num1 = random.randint(0, RANDOM_CALC_MAX)
+    rndm_num2 = random.randint(0, RANDOM_CALC_MAX)
+    rndm_oper = random.choice(['+', '-', '*'])
+    right_answer = str(get_result(rndm_num1, rndm_num2, rndm_oper))
+    question = 'Question: {0} {1} {2}'.format(rndm_num1, rndm_oper, rndm_num2)
+    return (question, right_answer)
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,37 +1,13 @@
 #!/usr/bin/env python
 """Calc programm."""
 
-import random
-
-import prompt
-from brain_games.games.calc import get_result
-from brain_games.games_module import (
-    RIGHT_ANSWER_NEEDED,
-    check_answer,
-    welcome_user,
-)
-
-RANDOM_CALC_MAX = 20
+from brain_games.games import calc
+from brain_games.games_module import engine
 
 
 def main():
     """Check user knowlenge in simple math."""
-    print('Welcome to the Brain Games!')
-    user_name = welcome_user()
-    print('What is the result of the expression?')
-    right_answer_count = 0
-    while right_answer_count < RIGHT_ANSWER_NEEDED:
-        rndm_num1 = random.randint(0, RANDOM_CALC_MAX)
-        rndm_num2 = random.randint(0, RANDOM_CALC_MAX)
-        rndm_oper = random.choice(['+', '-', '*'])
-        right_answer = str(get_result(rndm_num1, rndm_num2, rndm_oper))
-        print('Question: {0} {1} {2}'.format(rndm_num1, rndm_oper, rndm_num2))
-        user_answer = prompt.string('Your answer: ')
-        if check_answer(user_answer, right_answer, user_name):
-            right_answer_count += 1
-        else:
-            return
-    print('Congratulations, {0}!'.format(user_name))
+    engine(calc)
 
 
 if __name__ == '__main__':
