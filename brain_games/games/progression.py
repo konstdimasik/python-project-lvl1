289c960986d42cfd8a295f15a4ba41d40f9da7e9
Dmitry Konstantinov <konstdimasik@gmail.com>
2021-04-22 00:47:21 +0300

    Clean make_progression and make constants for progression paramaters

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,20 +2,26 @@
 
 import random
 
+PROG_LENGTH_MIN = 5
+PROG_LENGTH_MAX = 10
+PROG_START_MIN = 1
+PROG_START_MAX = 10
+PROG_STEP_MIN = 1
+PROG_STEP_MAX = 5
 RULE = 'What number is missing in the progression?'
 
 
-def make_progression():
+def make_progression(length, start, step):
     """Make progression with random length start and step."""
-    length = random.randint(5, 10)
-    start = random.randint(1, 10)
-    step = random.randint(1, 5)
     return [str(start + step * i) for i in range(length)]
 
 
 def generate_round():
     """Progression game core."""
-    progression = make_progression()
+    length = random.randint(PROG_LENGTH_MIN, PROG_LENGTH_MAX)
+    start = random.randint(PROG_START_MIN, PROG_START_MAX)
+    step = random.randint(PROG_STEP_MIN, PROG_STEP_MAX)
+    progression = make_progression(length, start, step)
     empty = random.randint(0, len(progression) - 1)
     right_answer = str(progression[empty])
     progression[empty] = '..'
